c18f4ef874398d535f3ff2d0b8f6e0c58742b405
JhonatanMuniz <jhonatanmuniz123@gmail.com>
2021-04-05 16:04:20 -0300

    Update features

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -2,11 +2,20 @@ import { Request, Response } from "express";
 
 import { ListAllUsersUseCase } from "./ListAllUsersUseCase";
 
+
 class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { user_id } = request.headers;
+
+    try {
+      const allUsers = this.listAllUsersUseCase.execute({ user_id: String(user_id) });
+      return response.json(allUsers);
+
+    } catch (error) {
+      return response.status(400).json({error: 'User does not exist'});
+    }
   }
 }
 
--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminUseCase.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminUseCase.ts
@@ -9,7 +9,15 @@ class TurnUserAdminUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User {
-    // Complete aqui
+    const user = this.usersRepository.findById(user_id);
+
+    if(!user) {
+      throw new Error("User does not exist");
+    }
+
+    const updateUser = this.usersRepository.turnAdmin(user);
+
+    return updateUser;
   }
 }
 
